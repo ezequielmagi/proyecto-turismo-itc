4f0101173f660c7a151e6f8b1be754ac1ae7a313
Ezequiel Magi <ezequielmaggi@gmail.com>
2023-06-06 10:44:15 -0300

    mejorando la landing

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# proyecto-turismo-itc
+# Proyecto del curso de Desarrollo WEB 
+
+## Objetivo
+
+Crear una página web utilizando [Tailwindcss](https://tailwindcss.com). La idea es proyectar los conocimientos adquiridos en las clases y elaborar una página responsive, con un diseño elaborado por el profesor.
+
+## Resultado
+
+Para ver la página web en funcionamiento, acceder al siguiente [link](https://travel-itc.vercel.app).
+
+## Desarrolladores
+
+[jaglux12](https://github.com/jaglux12)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,64 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    screens:{
+      'mobile': '375px',
+      'tablet': '768px',
+      'desktop': '1440px'
+    },
     extend: {
 
-      backgroundImage: {
-        'iguazu': "url('./src/img/puerto_iguazu.jpeg')"
+      colors: {
+        'red-travel':'#CC2D4A',
+        'green-travel':'#8FA206',
+        'blue-travel':'#61AEC9',
+        'gray-travel':'#faf9f9',
+        'dark-gray-travel':'#d3d3d3'
       },
 
-      backgroundColor: theme => ({
-        ...theme('colors'),
-        'rojo-itc': '#CC2D4A',
-        'verde-itc': '#8FA206',
-        'azul-itc': '#61AEC9'
-      }),
-      textColor: {
-        'rojo-itc': '#CC2D4A',
-        'verde-itc': '#8FA206',
-        'azul-itc': '#61AEC9'
+      textColor:{
+        'red-travel':'#CC2D4A',
+        'green-trabe':'#8FA206',
+        'blue':'#61AEC9'
+      },
+
+      backgroundImage: {
+        'iguazu':'url(https://i.postimg.cc/wMf9bKLW/Any-Conv-com-puerto-iguazu.webp)',
+        'isla':'url(https://i.postimg.cc/9MBmZwMH/Any-Conv-com-isla92.webp)',
+        'cafayate':'url(https://i.postimg.cc/W1Q1vPKC/Any-Conv-com-cafayate.webp)',
+        'calafate':'url(https://i.postimg.cc/C10MRsw-4/Any-Conv-com-calafate.webp)',
+        'bariloche':'url(https://i.postimg.cc/BZgb1kLP/Any-Conv-com-800x500px-Bariloche2-A.webp)',
+        'mendoza':'url(https://i.postimg.cc/WpXp4ZCM/Any-Conv-com-mendoza.webp)',
+        'cordoba':'url(https://i.postimg.cc/N0mfGzvM/Any-Conv-com-cordob.webp)',
+        'madryn':'url(https://i.postimg.cc/JzyzvhTj/Any-Conv-com-madry.webp)',
+        'chocon':'url(https://i.postimg.cc/wM0B8R8Q/Any-Conv-com-chocon-WEB-770x513.webp)'
+      },
+
+      spacing: {
+        'width-card-rec':'154px',
+        'height-card-rec':'215px',
+        'height-card-full':'402px',
+        'width-card-full':'334px',
+        'height-header':'73px',
+        'wheader-title-tablet':'550px',
+        'height-cafayate-tb':'830px',
+        'height-caf2-tb':'403px'
+      },
+
+      fontFamily: {
+        Montserrat: [ 'Montserrat', 'sans-serif' ]
+      },
+
+      screens:{
+        'min-laptop':'880px'
+      },
+
+      gridAutoRows:{
+        'row-3':'minmax(0,0.5fr)'
       }
+
     },
-    fontFamily: {
-      Montserrat: [ 'Montserrat', 'sans-serif' ]
-    }
   },
   plugins: [],
 }
